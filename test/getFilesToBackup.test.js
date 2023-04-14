73f4513577a021071beab884d49b553378e400a4
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-04-14 12:33:37 -0300

    add test

--- test/getFilesToBackup.test.js
+++ test/getFilesToBackup.test.js
@@ -1,37 +1,4 @@
-/*
-Para evitar perder datos cuando el servidor se cae, Papa Noel ha decidido hacer backups incrementales. Un hacker llamado S4vitelf le esta ayudando.
-
-Por un lado, tenemos el timestamp de cuándo se hizo el último backup.
-
-También tenemos los cambios que se han realizado en un array de arrays. Cada array interno contiene dos elementos: el id del archivo modificado y el timestamp de la modificación.
-
-Tienes que crear un programa que devuelva un array con las id de los archivos que tendríamos que hacer backup porque han sido modificados desde el último backup y ordenados de forma ascendente. Ejemplo:
-
-const lastBackup = 1546300800
-const changes = [
-  [ 3, 1546301100 ],
-  [ 2, 1546300800 ],
-  [ 1, 1546300800 ],
-  [ 1, 1546300900 ],
-  [ 1, 1546301000 ]
-]
-
-getFilesToBackup(lastBackup, changes) // => [ 1, 3 ]
-// El archivo con id 1 ha sido modificado dos veces
-// después del último backup.
-
-// El archivo con id 2 no ha sido modificado después
-// del último backup.
-
-// El archivo con id 3 ha sido modificado una vez
-// después del último backup.
-
-// Tenemos que hacer una copia de seguridad
-// de los archivos 1 y 3.
- */
-
-import { isArray } from 'mathjs'
-import { test, it, describe, expect, assert } from 'vitest'
+import { it, describe, expect } from 'vitest'
 
 const lastBackup = 1546300800
 
@@ -43,28 +10,52 @@ const changes = [
   [1, 1546301000]
 ]
 const getFilesToBackup = (last, change) => {
-  if (!isArray(change)) throw new Error('No is array')
   if (Number.isNaN(last)) throw new Error('No is a number')
 
-  return []
+  const reducer = change.reduce((acc, prev) => {
+    const [id, lastUpdate] = prev
+
+    if (lastUpdate > last && !acc.includes(id)) {
+      acc.push(id)
+    }
+
+    return acc
+  }, []).sort((a, b) => a - b)
+
+  return reducer
 }
 
-describe.skip('Skippe suite', () => {
-  test.length('sqrt', () => {
-    assert.equal(Math.sqrt(4), 3)
-  })
-})
-
 describe('getFilesToBackup', () => {
-  it('Should be a function', () => {
-    expect(typeof getFilesToBackup).toBe('function')
+  it('should return an empty array if no files were modified', () => {
+    const lastBackup = 1620571200
+    const changes = [
+      [1, 1620484800],
+      [2, 1620398400],
+      [3, 1620312000]
+    ]
+
+    const filesToBackup = getFilesToBackup(lastBackup, changes)
+    expect(filesToBackup).toEqual([])
   })
 
-  it('Should a throw is not a array', () => {
-    expect(() => getFilesToBackup({}, 2)).toThrowError()
+  it('should return an empty array if all files were modified before the last backup', () => {
+    const lastBackup = 1620571200
+    const changes = [
+      [1, 1620484800],
+      [2, 1620398400],
+      [3, 1620312000]
+    ]
+    const filesToBackup = getFilesToBackup(lastBackup, changes)
+    expect(filesToBackup).toEqual([])
   })
 
-  it('Should be an array', () => {
-    expect(getFilesToBackup(lastBackup, changes)).toStrictEqual([])
+  it('should return an array with the ids of files modified after the last backup', () => {
+    const filesToBackup = getFilesToBackup(lastBackup, changes)
+    expect(filesToBackup).toEqual([1, 3])
+  })
+
+  it('should return a sorted array of file ids', () => {
+    const filesToBackup = getFilesToBackup(lastBackup, changes)
+    expect(filesToBackup).toEqual([1, 3])
   })
 })
