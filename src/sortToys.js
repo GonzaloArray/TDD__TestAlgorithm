1fce34b6675d0e6f21a81c66fe5a3d1c9c73e66a
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-04-15 00:17:59 -0300

    add test

--- src/sortToys.js
+++ src/sortToys.js
@@ -1,5 +1,5 @@
 export const sortToys = (toys, positions) => {
-  const posX = positions.map(v => v).sort((a, b) => a - b)
+  const posX = [...positions].sort((a, b) => a - b)
 
   const result = posX.map((ye) => {
     return positions
